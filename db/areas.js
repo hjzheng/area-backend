c5d74e4ca35bb125b2032bb2860471f00d4cbbb1
hjzheng <zhenghaoju700@163.com>
2017-01-03 14:31:34 +0800

    不过滤例如市辖区,市,县等特殊地址,严格遵循国家区县地址设定

--- db/areas.js
+++ db/areas.js
@@ -18,7 +18,7 @@
 
 let connection = require('../common/db/connection');
 
-let specialIds = ['110000', '120000', '500000', '310000'];
+// let specialIds = ['110000', '120000', '500000', '310000'];
 
 function search(keyword, limit){
 	let sql = `select id, address as name from (
@@ -30,7 +30,7 @@ function search(keyword, limit){
 	
 			select c.cityID as id, concat(p.province, ' > ' ,c.city) as address 
 			from province p 
-			right join city c on p.provinceID = c.fatherID where c.city != '县' & c.city != '市辖区' & c.city != '市'
+			right join city c on p.provinceID = c.fatherID
 	
 			union all
 	
@@ -55,11 +55,11 @@ function search(keyword, limit){
 
 function city(provinceId) {
 	let sql = `select cityID as id, city as name from city where fatherID = ${provinceId}`;
-
-	if (specialIds.indexOf(provinceId) !== -1) {
-		sql = `select a.areaID as id, a.area as name from 
-		(select cityID from city where fatherID = ${provinceId}) as c left join area a on c.cityID = a.fatherID`;
-	}
+	//
+	// if (specialIds.indexOf(provinceId) !== -1) {
+	// 	sql = `select a.areaID as id, a.area as name from
+	// 	(select cityID from city where fatherID = ${provinceId}) as c left join area a on c.cityID = a.fatherID`;
+	// }
 
 	return connection.createStatement(sql);
 }
